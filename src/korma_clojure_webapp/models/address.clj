a7bc196d7dcad8f3e49770cebb895eda172be416
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-09-30 14:02:37 +0530

    remove separate model files

--- src/korma_clojure_webapp/models/address.clj
+++
@@ -1,7 +0,0 @@
-(ns korma-clojure-webapp.models.address
-  (:require [korma-clojure-webapp.db.entities :as db])
-  (:use korma.core))
-
-(defn insert-address [addrs]
-  (insert db/address
-          (values addrs)))
