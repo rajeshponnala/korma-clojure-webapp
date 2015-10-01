38153ad8de22bc142d79c6523264484be1371033
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-10-01 17:13:38 +0530

    add GET,POST routes for user model

--- src/korma_clojure_webapp/handler.clj
+++ src/korma_clojure_webapp/handler.clj
@@ -3,13 +3,25 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.json :as ring-json]
-            [ring.util.response	:as rr]))
+            [ring.util.response	:as rr]
+            [korma-clojure-webapp.models.model :refer [insert-account get-user
+                                                       insert-user get-all
+                                                       insert-address]]))
 
 (defroutes app-routes
-  (GET "/" [] "hello world")
+  (GET "/" [] (get-all))
+  (GET "/user/:id" [id]
+       (rr/response (get-user (read-string id))))
+  (POST "/user" {body :body}
+        (rr/response (insert-user body)))
+  (POST "/account" {body :body}
+        (rr/response (insert-account body)))
+  (POST "/user/:id/address" {body :body}
+        (rr/response (insert-address body)))
   (route/not-found "Not Found"))
 
 (def app
   (-> app-routes
       (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
+      (ring-json/wrap-json-body)
       (ring-json/wrap-json-response)))
